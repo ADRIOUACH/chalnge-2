135faddce759ae6a6af8b24a1c9202b73696c4b7
ADRIOUACH <jawadadriouach.com>
2023-09-15 11:45:35 +0100

    Challenge1

---
+++ main1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nom[10];
+    char prenom[12];
+    char sex[10];
+
+    printf("entre votre nom :");
+    scanf("%s",&nom);
+        printf("entre votre prenom  :");
+    scanf("%s",&prenom);
+           printf("entre votre sex :");
+    scanf("%s",&sex);
+     int age;
+     int  telephon;
+           printf("entre votre age :");
+    scanf("%d",&age);
+           printf("entre votre telephon :");
+    scanf("%d",&telephon);
+
+
+    return 0;
+}
