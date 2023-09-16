ab84af5083d256a80955472cd2fe9e67276ea607
ADRIOUACH <jawadadriouach.com>
2023-09-16 15:40:04 +0100

    chalange 6

---
+++ main6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+double circonference;
+    double r;
+    printf(" entre le rayon du cercle");
+    scanf("%lf",&r);
+    circonference = 2*3.14*r;
+    printf("la circonference du cercle = %.2f",circonference);
+
+    return 0;
+}
