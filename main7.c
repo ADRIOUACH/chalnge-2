cc4b6ce1902bce5a443c7121fce6087ed6284c68
ADRIOUACH <jawadadriouach.com>
2023-09-16 16:10:28 +0100

    chlange 7

---
+++ main7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float N1,N2,N3,X;
+    printf("entre N1");
+    scanf("%f",&N1);
+    printf("entre N2");
+    scanf("%f",&N2);
+    printf("entre N3");
+    scanf("%f",&N3);
+
+     printf("%f %f %f :",N3,N2,N1);
+
+    return 0;
+}
